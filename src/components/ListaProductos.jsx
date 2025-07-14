8579363a16e61f9b869fc9cadd1ee0a04589af3a
CamilaSantis <camila.santis03@inacapmail.cl>
2025-07-14 18:00:34 -0400

    mejorar estilos de tarjeta de producto y corregir importación en ListaProductos

--- src/components/ListaProductos.jsx
+++ src/components/ListaProductos.jsx
@@ -1,13 +1,16 @@
-import ProductoCard from "./productossugeridos.jsx";
+import React from "react";
+import ProductoCard from "./ProductoCard.jsx";
 
 export default function ListaProductos({ productos, onAgregar }) {
-  console.log("Recibiendo productos:", productos);
-
   return (
     <div id="contenedor-productos">
       {productos.length > 0 ? (
         productos.map((prod) => (
-          <ProductoCard key={prod.id} producto={prod} onAgregar={onAgregar} />
+          <ProductoCard
+            key={prod.id}
+            producto={prod}
+            onAgregar={onAgregar}
+          />
         ))
       ) : (
         <p>No se encontraron productos.</p>
